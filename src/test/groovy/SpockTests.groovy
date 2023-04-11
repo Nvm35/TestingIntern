359b251736f52b024d669fe906c2397e9d66df99
Кондратьев Никита Сергеевич <alan.lake@mail.ru>
2023-04-11 18:06:11 +0300

    clean imports

--- src/test/groovy/SpockTests.groovy
+++ src/test/groovy/SpockTests.groovy
@@ -3,11 +3,7 @@ import com.testingintern.entity.Person
 import com.testingintern.repos.LocationRepository
 import com.testingintern.repos.PersonRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
