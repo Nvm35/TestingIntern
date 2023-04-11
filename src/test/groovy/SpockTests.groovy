079a6b0cdc5e05156e4e9e80207ca51da9e14c6a
Nvm35 <twikk3@gmail.com>
2023-04-11 23:17:32 +0600

    fixed

--- src/test/groovy/SpockTests.groovy
+++ src/test/groovy/SpockTests.groovy
@@ -1,14 +1,13 @@
+import com.testingintern.TestingInternApplication
 import com.testingintern.entity.Person
 import com.testingintern.repos.LocationRepository
 import com.testingintern.repos.PersonRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.web.servlet.MockMvc
+import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
+@SpringBootTest(classes = TestingInternApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpockTests extends Specification {
-
-    @Autowired
-    MockMvc mockMvc
 
     @Autowired
     PersonRepository personRepository
@@ -16,22 +15,21 @@ class SpockTests extends Specification {
     @Autowired
     LocationRepository locationRepository
 
-
-    def "should create and retreve a person, I need Henry"() {
+    def "should create and retrieve a person, I need Henry"() {
         given:
-        def person = new Person(name: "Henry Cavill", email: "superman@dc.com")
-
+        def person = new Person(name: "Henry Cavill", email: "superman@dc.com",id: 4)
 
         when:
         personRepository.save(person)
+        List<Person> people =personRepository.findAll()
 
         then:
-        person.getName()
-        println person.email
+        println "All people in PersonRepository:"
+        people.each { println "${it.name}, ${it.email}" }
     }
 
     def "what"() {
         expect:
-        1==1
+        1 == 1
     }
 }
