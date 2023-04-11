cc508e656d0c8eba1c522e3e0bb9e2653dec0b23
Nvm35 <twikk3@gmail.com>
2023-04-11 20:28:36 +0600

    add groovy, tests

---
+++ src/test/groovy/SpockTests.groovy
@@ -0,0 +1,37 @@
+import com.testingintern.entity.Person
+import com.testingintern.repos.LocationRepository
+import com.testingintern.repos.PersonRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+class SpockTests extends Specification {
+
+    @Autowired
+    MockMvc mockMvc
+
+    @Autowired
+    PersonRepository personRepository
+
+    @Autowired
+    LocationRepository locationRepository
+
+
+    def "should create and retreve a person, I need Henry"() {
+        given:
+        def person = new Person(name: "Henry Cavill", email: "superman@dc.com")
+
+
+        when:
+        personRepository.save(person)
+
+        then:
+        person.getName()
+        println person.email
+    }
+
+    def "what"() {
+        expect:
+        1==1
+    }
+}
